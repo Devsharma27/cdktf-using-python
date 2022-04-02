40e6404f1a5bc7b54886e03315c4f62a9c32061b
Devsharma27 <94638730+Devsharma27@users.noreply.github.com>
2022-04-02 14:22:00 +0530

    Create user_data.sh

---
+++ user_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#!/usr/bin/env bash
+sudo yum update -y
+sudo yum install python3-pip git mysql -y
+rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo aws s3 cp s3://cloudwatchogs/config.json /opt/aws/amazon-cloudwatch-agent/config.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/config.json
+sudo /bin/systemctl restart amazon-cloudwatch-agent.service
+sudo git clone "https://github.com/Devsharma27/oneflask.git"
+sudo pip3 install flask
+sudo yum -y install python python3-devel mysql-devel redhat-rpm-config gcc
+sudo pip3 install flask_mysqldb
+sudo pip3 install mysql-connector-python
+cd oneflask
+python3 app.py
+sudo /bin/systemctl restart amazon-cloudwatch-agent.service
